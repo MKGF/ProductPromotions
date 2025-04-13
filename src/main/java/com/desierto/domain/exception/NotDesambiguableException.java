161a9760b17303e45350584019a12a22827d621b
Mikel Garín <mikelgarin@gmail.com>
2025-04-13 23:38:37 +0200

    Delegated desambiguation to the query

--- src/main/java/com/desierto/domain/exception/NotDesambiguableException.java
+++
@@ -1,3 +0,0 @@
-package com.desierto.domain.exception;
-
-public class NotDesambiguableException extends PriceException {}
--- src/test/java/com/desierto/application/service/GetPriceForADateServiceTest.java
+++ src/test/java/com/desierto/application/service/GetPriceForADateServiceTest.java
@@ -4,13 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-import com.desierto.application.exception.ConflictingPricesException;
 import com.desierto.domain.Price;
-import com.desierto.domain.exception.PriceException;
-import com.desierto.domain.exception.PriceNotFoundException;
 import com.desierto.domain.repository.PriceRepository;
 import java.time.LocalDateTime;
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -31,52 +27,10 @@ class GetPriceForADateServiceTest {
   }
 
   @Test
-  public void givenADate_forVariousPrices_returnsAPrice() throws PriceException {
-    // Given
-    Price aHighPriorityPrice = new Price(3);
-    Price aLowerPriorityPrice = new Price(2);
-    Price anEvenLowerPriorityPrice = new Price(1);
-    Price lowestPriorityPrice = new Price(0);
-    List<Price> prices = List.of(aLowerPriorityPrice, anEvenLowerPriorityPrice, lowestPriorityPrice, aHighPriorityPrice);
-    when(priceRepository.findByDate(any())).thenReturn(prices);
-
-    // When
-    Price result = cut.execute(LocalDateTime.now());
-
-    // Then
-    assertEquals(aHighPriorityPrice, result);
-  }
-
-  @Test
-  public void givenADate_forNoPrices_throwsException() {
-    // Given
-    List<Price> prices = List.of();
-    when(priceRepository.findByDate(any())).thenReturn(prices);
-
-    // When Then
-    assertThrows(PriceNotFoundException.class, () -> cut.execute(LocalDateTime.now()));
-  }
-
-  @Test
-  public void givenADate_forVariousPricesWithSamePriority_throwsException() {
-    // Given
-    Price aHighPriorityPrice = new Price(3);
-    Price anotherHighPriorityPrice = new Price(3);
-    Price aLowerPriorityPrice = new Price(1);
-    Price lowestPriorityPrice = new Price(0);
-    List<Price> prices = List.of(aLowerPriorityPrice, anotherHighPriorityPrice, lowestPriorityPrice, aHighPriorityPrice);
-    when(priceRepository.findByDate(any())).thenReturn(prices);
-
-    // When Then
-    assertThrows(ConflictingPricesException.class, () -> cut.execute(LocalDateTime.now()));
-  }
-
-  @Test
-  public void givenADate_forAPrice_returnsAPrice() throws PriceException {
+  public void givenADate_forVariousPrices_returnsAPrice() {
     // Given
     Price aPrice = new Price(1);
-    List<Price> prices = List.of(aPrice);
-    when(priceRepository.findByDate(any())).thenReturn(prices);
+    when(priceRepository.findByDate(any())).thenReturn(aPrice);
 
     // When
     Price result = cut.execute(LocalDateTime.now());
